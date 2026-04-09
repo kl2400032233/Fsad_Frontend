a6144f7354b612c7ee97b093b5d7307fdd48f80e
Yashi <2400032233@kluiversity.in>
2026-04-09 11:04:54 +0530

    removed code for understaning

--- app/dashboard/donor/donations/page.jsx
+++ app/dashboard/donor/donations/page.jsx
@@ -51,10 +51,7 @@ export default function DonationsPage() {
                 Organization:{" "}
                 <span className="text-white">{d.organizationName}</span>
               </p>
-              <p className="text-gray-400 text-center mt-10">
-  No donations available 🚫
-</p>
-// Fetch donations from backend API
+              
 
               {/* Status Badge */}
               <span className="inline-block mt-3 px-3 py-1 text-sm rounded-full bg-yellow-500/20 text-yellow-400">
